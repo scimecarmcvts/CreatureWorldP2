9817104669856bca2aab7f6ec5f5527f4e7c5e7a
Shrish-MCVTS <Shrish.kushwaha@mcvts.org>
2025-06-02 10:40:26 -0400

    Update Mouse.js

--- Mouse.js
+++ Mouse.js
@@ -1,41 +1,49 @@
 class Mouse {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.position = createVector(x,y);
+    this.velocity = createVector(random(-3,3),0)
+    this.acceleration = createVector(0,0)
     this.img = loadImage("assets/MouseRIGHT.png");
     this.visibility = true;
     this.size = 32;
     this.speed = 3;
+    this.flip = true;
     //this.sound = loadSound("assets/MouseName.mp3");
-    this.xoff = random(1000);
-    this.counter = 0;
   }
   show() {
-    if (this.counter < 4) {
-      if (this.visibility)
-        image(this.img, this.x, this.y, this.size, this.size);
-    } else {
+    
+    if (this.visibility && this.flip){
+    image(this.img, this.position.x, this.position.y, this.size, this.size);
+    } else{
       push();
       scale(-1, 1);
-      image(this.img, -this.x, this.y, this.size, this.size);
+      image(this.img, -this.position.x, this.position.y, this.size, this.size);
       pop();
-    }
-    this.counter += 0.01;
-    if (this.counter > 11) {
-      this.counter = 0;
     }
     // if (this.collision(other)) {
     //   this.playSound();
     // }
   }
+
   move() {
-    this.x = noise(this.xoff) * width;
+    this.velocity.limit(15,15)
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
     this.xoff += 0.002 * this.speed;
     if (millis() % 10000 == 0) this.speed = random(3, 5);
-    //Need to change other to another object
+    this.checkEdges()
   }
+
+  checkEdges(){
+    if (this.position.x > width - 12 || this.position.x < 12) {
+      this.velocity.x *= -1;
+      this.acceleration.x *= -1;
+      this.flip = !this.flip
+    }
+  }
+
   collision(other, distance) {
-    return dist(this.x, this.y, other.x, other.y) < distance;
+    return dist(this.position.x, this.position.y, other.position.x, other.position.y) < distance;
   }
   // playSound() {
   //   song.play();
