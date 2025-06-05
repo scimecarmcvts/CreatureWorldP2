716ad8829cb773753fabb4e354c6405828daa6a0
GabrielArias22 <gabriel.arias@mcvts.org>
2025-06-05 10:03:26 -0400

    Update Mushroom.js

--- Mushroom.js
+++ Mushroom.js
@@ -9,31 +9,35 @@ class Mushroom {
     this.flipped = false;
     this.yoff = random(0, 50);
     this.y;
+    this.position = createVector(100, 100);
+
     noiseSeed(random(5000));
   }
   show() {
-    let y = noise(this.yoff+1000) * height + 200; 
+    this.position.x = noise(this.yoff+999) * height + 200; 
+    this.position.y = noise(this.yoff+1000) * height + 200; 
     if (this.flipped) {
       push();
       scale(-1, 1);
-      image(this.img, this.x, y, 32, 32);
+      image(this.img, this.position.x, this.position.y, 32, 32);
       pop();
-    } else{ image(this.img, this.x, y, 32, 32);
+    } else{ image(this.img, this.position.x, this.position.y, 32, 32);
  }
   }
    move(){
-this.x+= -2*this.dir;
-this.y += 0.04;
+
+this.position.x+= -2*this.dir;
+this.position.y += 0.04;
 this.yoff += 0.04;
 
-    if (this.x < -100 ) {
+    if (this.position.x < -400 ) {
       this.flipped=true;
 
     }
-      if (this.x < -450 ) {
+      if (this.position.x < -750 ) {
         
       this.flipped=false;
- this.x=width-100;
+ this.position.x=width-100;
     }
    } 
  
